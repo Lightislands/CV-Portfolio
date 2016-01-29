dae1dc737d76e2e389dd02d7185046b8780c9558
Lightislands <cyprusnetmail@gmail.com>
2016-01-29 19:10:08 +0100

    Files uploading

---
+++ js/agency.js
@@ -0,0 +1,62 @@
+/*!
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// LightBox - http://ashleydw.github.io/lightbox/#single-image
+            $(document).ready(function ($) {
+                // delegate calls to data-toggle="lightbox"
+                $(document).delegate('*[data-toggle="lightbox"]:not([data-gallery="navigateTo"])', 'click', function(event) {
+                    event.preventDefault();
+                    return $(this).ekkoLightbox({
+                        onShown: function() {
+                            if (window.console) {
+                                return console.log('Checking our the events huh?');
+                            }
+                        },
+						onNavigate: function(direction, itemIndex) {
+                            if (window.console) {
+                                return console.log('Navigating '+direction+'. Current item: '+itemIndex);
+                            }
+						}
+                    });
+                });
+            });
+			
+
+//Scroll-page-up
+ var top_show = 400; // In what position of scroll bars to start showing the "Top"
+  var delay = 300; // Scroll delay
+  $(document).ready(function() {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > top_show) $('#top_scr').fadeIn();
+      else $('#top_scr').fadeOut();
+    });
+    $('#top_scr').click(function () {
+      $('body, html').animate({
+        scrollTop: 0
+      }, delay);
+    });
+  });
